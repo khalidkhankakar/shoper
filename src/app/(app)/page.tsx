b7c696bf4854ac5330cdad5742c93b3361216e43
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-26 19:51:40 +0500

    Setup tailwind css

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">This is a sample paragraph.</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">Click Me</button>
+    </div>
+  )
+}
+
+export default page
