d835a6be0def9b0c8c6f0f1724c4086d58e4e351
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-29 17:00:33 +0500

    🎉 implement viewall category and dropdown of it

--- src/app/(app)/(home)/_components/search-filter/category-dropdown.tsx
+++ src/app/(app)/(home)/_components/search-filter/category-dropdown.tsx
@@ -1,48 +1,43 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import Link from "next/link"
-
+} from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
+import { CustomCategory } from '../../types'
 
 interface Props {
-  category: any,
-  isActive?: boolean,
-  isNavigationHover?: boolean,
+  category: CustomCategory[]
+  isActive?: boolean
+  isNavigationHover?: boolean
 }
 
 export const CategoryDropdown = ({ category, isActive, isNavigationHover }: Props) => {
-
   const backgroundColor = category.color || '#32a852'
 
-  return (  
+  return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="rounded-full px-4 py-1 " variant={isActive ? 'evaluated' : 'outline'}>{category.name}</Button>
+        <Button className="rounded-full px-4 py-1 " variant={isActive ? 'evaluated' : 'outline'}>
+          {category.name}
+        </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent style={{backgroundColor}} className="w-56 ">
-        <DropdownMenuGroup >
-
-          {
-            category.subcategories.map((subcat) => (
-              <DropdownMenuItem key={subcat.id} className="hover:bg-black hover:text-white py-2 w-full text-lg font-medium underline cursor-pointer" asChild>
-                <Link href={'/'}>
-                  {subcat.name}
-                </Link>
-              </DropdownMenuItem>
-            ))
-          }
+      <DropdownMenuContent style={{ backgroundColor }} className="w-56 ">
+        <DropdownMenuGroup>
+          {category.subcategories.map((subcat) => (
+            <DropdownMenuItem
+              key={subcat.id}
+              className="hover:bg-black hover:text-white py-2 w-full text-lg font-medium underline cursor-pointer"
+              asChild
+            >
+              <Link href={'/'}>{subcat.name}</Link>
+            </DropdownMenuItem>
+          ))}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   )
 }
-
-
-
-
-
